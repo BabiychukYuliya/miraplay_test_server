a3cd143fdd937801e51d6cc66aaf465a8ee18706
BabiychukYuliya <julia4233097@gmail.com>
2023-11-26 23:48:35 +0200

    string for db

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+// julia
+// 1lPVOxIXeA1LF8Js
+// const DB_HOST = 'mongodb+srv://julia:1lPVOxIXeA1LF8Js@cluster0.azoxse6.mongodb.net/miraplay_user?retryWrites=true&w=majority'
 
 // app.get("/", (req, res) => {
 //   res.send("<h2>Home page</h2>");
